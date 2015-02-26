97553dcd24529e4c86be08140476f9689f42a023
Andrew Chung <andrew.chung159@gmail.com>
2015-02-25 19:52:29 -0500

    Created breadth-first search and depth-first search methods.

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,26 +1,5 @@
-class BinaryTree
-  attr_accessor :contents
-
-  def initialize(arr)
-    @contents = build_tree(arr)
-  end
-
-  def build_tree(arr)
-    root = Node.new(arr.shift)
-    arr.each { |value| add_node(Node.new(value, parent = root), root) }
-  end
-
-  def add_node(node, parent)
-    if node.value <= parent.value
-      parent.lchild.nil? ? parent.lchild = node : add_node(node, parent.lchild)
-    else
-      parent.rchild.nil? ? parent.rchild = node : add_node(node, parent.rchild)
-    end
-  end
-end
-
 class Node
-  attr_accessor :value, :parent, :lchild, :rchild
+  attr_accessor :value, :lchild, :rchild, :parent
 
   def initialize(value, parent = nil, lchild = nil, rchild = nil)
     @value = value
@@ -30,5 +9,56 @@ class Node
   end
 end
 
-tree = BinaryTree.new([1, 7, 4, 23, 8, 9, 4])
-p tree.contents
+def build_tree(arr)
+  root = Node.new(arr.shift)
+  arr.each { |value| insert(node = Node.new(value), root) }
+  root
+end
+
+def insert(node, parent)
+  node.parent = parent
+  if node.value <= parent.value
+    parent.lchild.nil? ? parent.lchild = node : insert(node, parent.lchild)
+  else
+    parent.rchild.nil? ? parent.rchild = node : insert(node, parent.rchild)
+  end
+end
+
+def breadth_first_search(value, tree)
+  queue = [tree]
+
+  until queue.empty?
+    node = queue.shift
+    next if node == nil
+    puts "Current node is #{node.value}."
+    return value if value == node.value
+    queue << node.lchild
+    queue << node.rchild
+  end
+
+   nil
+end
+
+def depth_first_search(value, tree)
+  stack = [tree]
+
+  until stack.empty?
+    node = stack.pop
+    next if node == nil
+    puts "Current node is #{node.value}."
+    return value if value == node.value
+    stack << node.lchild
+    stack << node.rchild
+  end
+
+  nil
+end
+
+def dfs_rec
+
+end
+
+tree = build_tree([1, 7, 4, 23, 8, 9, 4])
+p tree
+p breadth_first_search(4, tree)
+p depth_first_search(4, tree)
